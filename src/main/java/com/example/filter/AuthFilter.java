b38810c58d0e3f629f0796ec279102216648c9c6
victor diaz <vdmpeniel@yahoo.es>
2023-08-02 15:28:25 -0400

    Removed the dot in all paths and changed the login post method. Changed all variable names to those shown in the tests

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -16,16 +16,17 @@ public class AuthFilter implements Filter {
     private static final Logger logger = Logger.getLogger(AuthFilter.class.getName());
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         try {
             HttpSession session = ((HttpServletRequest) response).getSession();
             if (Objects.isNull(session.getAttribute("user"))) {
                 ((HttpServletResponse) response).sendRedirect("/login.jsp");
             }
+            filterChain.doFilter(servletRequest, response);
 
         } catch(Exception e) {
             logger.info("Error: " + e.getCause());
         }
-        chain.doFilter(request, response);
+
     }
 }
